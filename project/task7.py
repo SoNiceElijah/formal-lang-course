ff5ab3e56073051f851cf904c4c94adf4c874a48
Ilya <soniceelijah@yandex.ru>
2023-04-24 23:08:01 +0300

    Задача 8. Колегов (#7)

---
+++ project/task7.py
@@ -0,0 +1,138 @@
+from pyformlang.cfg import CFG, Variable, Terminal
+from pyformlang.regular_expression import Regex
+from pyformlang.finite_automaton import EpsilonNFA
+from scipy.sparse import dok_matrix
+from typing import Dict, Callable
+
+
+class TaskECFG:
+    """
+    Представление для ECFG
+    """
+
+    @staticmethod
+    def from_text(text: str):
+        """
+        Создает ECFG из текста
+        """
+
+        productions = {}
+
+        lines = text.splitlines()
+        lines = list(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), lines)))
+
+        for line in lines:
+            tokens = line.split("->")
+
+            name = tokens[0].strip()
+            body = tokens[1].strip()
+
+            if name in productions:
+                productions[name] = productions[name].union(Regex(body))
+            else:
+                productions[name] = Regex(body)
+
+        return TaskECFG(productions=productions)
+
+    @staticmethod
+    def from_file(path: str):
+        """
+        Создает ECFG из файла по пути path
+        """
+        with open(path) as f:
+            return TaskECFG.from_text("\n".join(f.readlines()))
+
+    @staticmethod
+    def from_cfg(cfg: CFG):
+        """
+        Создает ECFG из CFG
+        """
+        return TaskECFG.from_text(cfg.to_text())
+
+    def __init__(
+        self, /, productions: Dict[str, Regex] = None, start_symbol: str = None
+    ):
+        self._productions = productions if productions is not None else {}
+        self._start_symbol = "S" if start_symbol is None else start_symbol
+
+    @property
+    def productions(self):
+        return self._productions
+
+
+class TaskRFA:
+    """
+    Представление для recursive finite automaton
+    """
+
+    @staticmethod
+    def from_ecfg(ecfg: TaskECFG):
+        """
+        Создает RFA из ECFG
+        """
+        a = {}
+        for v, r in ecfg.productions.items():
+            a[v] = r.to_epsilon_nfa()
+        return TaskRFA(a)
+
+    @staticmethod
+    def from_text(text: str):
+        """
+        Создает RFA из текста
+        """
+        return TaskRFA.from_ecfg(TaskECFG.from_text(text))
+
+    @staticmethod
+    def from_file(path: str):
+        """
+        Создает RFA из файла
+        """
+        return TaskRFA.from_ecfg(TaskECFG.from_file(path))
+
+    @staticmethod
+    def from_procedure(func: Callable[[], TaskECFG]):
+        """
+        Создает RFA из ECFG, который возвращает переданная процедура
+        """
+        return TaskRFA.from_ecfg(func())
+
+    def __init__(self, automatus: Dict[str, EpsilonNFA]):
+        self._automatus = automatus
+
+    def minimize(self):
+        a = {}
+        for name, auto in self._automatus.items():
+            a[name] = auto.minimize()
+        return TaskRFA(a)
+
+    @property
+    def automatus(self):
+        return self._automatus
+
+    def get_matrix(self):
+        """
+        Возвращает матричное представление данного автомата
+        """
+
+        def convert(automatus: EpsilonNFA):
+            mapper = {}
+            n = 0
+            for idx, state in enumerate(automatus.states):
+                mapper[state] = idx
+                n += 1
+            edges_matrixes = {}
+            for a, e, b in automatus:
+                if e not in edges_matrixes:
+                    edges_matrixes[e] = dok_matrix((n, n), dtype=np.bool_)
+                edges_matrixes[e][mapper[a], mapper[b]] = np.bool_(True)
+
+            return (
+                edges_matrixes,
+                [mapper[x] for x in automatus.start_states],
+                [mapper[x] for x in automatus.states],
+            )
+
+        a = {}
+        for v, r in self._automatus.items():
+            a[v] = convert(r)
+        return a
---
+++ tests/test_task8.py
@@ -0,0 +1,52 @@
+from project.task6 import TaskCFG
+from project.task8 import make_query, hellings_algo
+from networkx import MultiDiGraph
+
+
+def test_can_do_simple_hellings_algo():
+
+    g = MultiDiGraph()
+
+    g.add_node(0)
+    g.add_node(1)
+    g.add_node(2)
+    g.add_node(3)
+
+    g.add_edge(0, 1, label="a")
+    g.add_edge(1, 2, label="b")
+    g.add_edge(2, 3, label="c")
+
+    grammar = "S -> A D\nD -> B C\nA -> a\nB -> b\nC -> c"
+    cfg = TaskCFG.from_text(grammar)
+
+    actual = hellings_algo(cfg, g)
+    expected = {(0, "S", 3), (1, "D", 3), (0, "A", 1), (1, "B", 2), (2, "C", 3)}
+
+    assert actual == expected
+
+
+def test_can_do_simple_query():
+
+    g = MultiDiGraph()
+
+    g.add_node(0)
+    g.add_node(1)
+    g.add_node(2)
+    g.add_node(3)
+    g.add_node(4)
+    g.add_node(5)
+    g.add_node(6)
+
+    g.add_edge(0, 1, label="x")
+    g.add_edge(1, 2, label="p")
+    g.add_edge(2, 3, label="x")
+    g.add_edge(3, 4, label="p")
+    g.add_edge(4, 5, label="x")
+    g.add_edge(5, 6, label="p")
+
+    grammar = "S -> X | X E\nX -> x\nE -> P S\nP -> p\n"
+    cfg = TaskCFG.from_text(grammar)
+
+    actual = make_query(g, [0], [3, 5, 6], cfg, "S")
+
+    assert actual == {(0, 3), (0, 5)}
